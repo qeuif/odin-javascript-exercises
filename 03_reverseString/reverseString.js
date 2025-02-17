d1f3bdab1973142253e7245cf94748ca206081d3
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-18 03:46:26 +0800

    Added my solutions to exercise-03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let endPointer = string.length - 1;
+    let res = "";
+    for (let i = endPointer; i >= 0; i--) {
+        res += string.charAt(i);
+    }
+    return res;
 };
 
 // Do not edit below this line
