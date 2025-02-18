e86a4360b1fb76967d5b1180b7a98796caaebcd8
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-19 02:58:09 +0800

    Added my solutions to exercise-07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let tempInCelsius = (tempInFahrenheit - 32) * (5/9) // formula to convert from fahrenheit to celsius
+  if (Number.isInteger(tempInCelsius)) {
+    return tempInCelsius;
+  }
+  return parseFloat(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFahrenheit = (tempInCelsius * (9/5) + 32); // formula to convert from celsius to fahrenheit
+  if (Number.isInteger(tempInFahrenheit)) {
+    return tempInFahrenheit;
+  }
+  return parseFloat(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
