c57108d7b361f138a34eaaca72cbbc5899aac9f0
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-18 13:53:56 +0800

    Added my solutions to exercise-05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let finalSum = 0;
 
+    let numbers = [int1,int2];
+    let checkElems = numbers.filter((num) => Number.isInteger(num) && num >= 0);
+    if (checkElems.length === 2) {
+        let start = Math.min(int1, int2);
+        let end = Math.max(int1, int2);
+        for (let i = start; i <= end; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
