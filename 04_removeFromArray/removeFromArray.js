c10c616dcc9d92fa857abfc6df3b1e5a242d8537
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-18 13:19:16 +0800

    Added my solutions to exercise-04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+    let res = [];
+    for (const item of array) {
+        let isMatch = false;
+        for (const elemToBeRemoved of removedValues) {
+            if (elemToBeRemoved === item) {
+                isMatch = true;
+            }
+        }
+        if (isMatch === false) {
+            res.push(item);
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
